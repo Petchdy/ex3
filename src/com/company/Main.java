cdca64c0dde03f6c7e8a621fdaf0c1158c273b5f
Petchdy <skulwisit@gmail.com>
2020-04-19 13:21:46 +0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int sum = 0;
+        int roundsum;
+        while (true){
+            int n = scan.nextInt();
+            if (n>0){
+                int price = scan.nextInt();
+                int take_away = scan.nextInt();
+                if (take_away==1){
+                    roundsum = (n*price)+10;
+                }
+                else {
+                    roundsum = n*price;
+                }
+                System.out.printf("%d \n",roundsum);
+                sum = sum+roundsum;
+            }
+            if(n<=0) {
+                break;
+            }
+        }
+        System.out.printf("%d",sum);
+    }
+}
